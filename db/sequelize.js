874f45dd23aae0f5ee9b7c896b395b18a366b892
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-02 17:57:37 -0500

    configure sequlize for production database

--- db/sequelize.js
+++ db/sequelize.js
@@ -6,7 +6,15 @@ const {DATABASE_URL, DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, SEQUEL
 
 console.log(`Connecting to database at ${DATABASE_URL}`);
 
-let sequelize = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, SEQUELIZE_OPTIONS);
+let sequelize;
+
+if (process.env.SIMPLIFY_DATABASE) {
+    sequelize = new Sequelize(process.env.SIMPLIFY_DATABASE, SEQUELIZE_OPTIONS);
+}else {
+    sequelize = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, SEQUELIZE_OPTIONS);
+};
+
+
 
 module.exports = {
     sequelize
