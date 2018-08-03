56e6d0b3710215dca4b279d8f522ba4150e400b4
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-03 07:18:24 -0500

    create category GET and POST endpoints

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const userRouter = require('./routes/users');
 const billsRouter = require('./routes/bills');
+const categoryRouter = require('./routes/categories');
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.use(express.json());
 
 app.use('/user', userRouter);
 app.use('/bills', billsRouter);
+app.use('/categories', categoryRouter);
 
 module.exports = app;
---
+++ routes/categories.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const {Category} = require('../models');
+
+router.get('/user/:userId', (req, res) => {
+
+    return Category.findAll({
+        where: {
+            user_id: req.params.userId
+        }
+    })
+    .then(categories => res.json({
+        categories: categories.map(category => category.apiRepr())
+    }))
+});
+
+router.post('/user/:userId', (req, res) => {
+    const requiredFields = ['category', 'amount'];
+
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+
+        if (!(field in req.body)) {
+            const message = `Missing ${field} in request body`;
+            console.error(message);
+
+            return res.status(400).send(message);
+        }
+    }
+
+    return Category.create({
+        category: req.body.category,
+        amount: req.body.amount,
+        user_id: req.params.userId
+    })
+    .then(category => res.status(201).json(category.apiRepr()))
+    .catch(err => res.status(500).send({message: 'Internal server error'}));
+});
+
+module.exports = router;
