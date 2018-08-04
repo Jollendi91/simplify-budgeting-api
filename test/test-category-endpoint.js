31dbe700e9cd882a622d8967e1c1ec0bd1a7d1de
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-04 14:42:54 -0500

    refactor Category GET integration test w/ auth

--- test/test-category-endpoint.js
+++ test/test-category-endpoint.js
@@ -9,13 +9,31 @@ const {User, Category} = require('../models');
 
 chai.use(chaiHttp);
 
+let authToken;
+let user;
+
 function seedUserData() {
-    return User.create({
-        firstName: faker.name.findName(),
+    user = {
+        firstName: faker.name.firstName(),
         lastName: faker.name.lastName(),
         username: faker.internet.userName(),
         password: faker.internet.password(),
+        setupStep: 1,
         monthlySalary: faker.finance.amount()
+    };
+
+    return chai.request(app)
+    .post('/simplify/users')
+    .send(user)
+    .then(res => {
+        user.id = res.body.id;
+        return chai.request(app)
+        .post('/simplify/auth/login')
+        .send({username: user.username, password: user.password});
+    })
+    .then(res => {
+        authToken = res.body.authToken;
+        return
     });
 }
 
@@ -34,7 +52,7 @@ function generateCategoryData(userId=null) {
 
 function seedData(seedNum=3) {
     return seedUserData()
-        .then(user => {
+        .then(() => {
             const promises = [];
             for (let i=0; i<seedNum; i++) {
                 promises.push(Category.create(generateCategoryData(user.id)));
@@ -56,35 +74,32 @@ describe('Category API resource', function() {
 
     describe('GET endpoint', function() {
         it('should return a users categories', function() {
-            let user;
+
             let resCategory;
 
-            return User.findOne()
-                .then(_user => {
-                    user = _user;
-                    return chai.request(app)
-                        .get(`/categories/user/${user.id}`);
-                })
-                .then(res => {
-                    res.should.have.status(200);
-                    res.should.be.json;
-                    res.body.categories.should.have.lengthOf(3);
+            return chai.request(app)
+            .get(`/simplify/categories`)
+            .set('Authorization', `Bearer ${authToken}`)
+            .then(res => {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.categories.should.have.lengthOf(3);
 
-                    res.body.categories.forEach(category => {
-                        category.should.be.an('object');
-                        category.should.include.keys('id', 'category', 'amount');
-                    });
-
-                    resCategory = res.body.categories[0];
-
-                    return Category.findById(resCategory.id);
-                })
-                .then(category => {
-                    category.id.should.equal(resCategory.id);
-                    category.category.should.equal(resCategory.category);
-                    category.amount.should.equal(resCategory.amount);
-                    category.user_id.should.equal(user.id);
+                res.body.categories.forEach(category => {
+                    category.should.be.an('object');
+                    category.should.include.keys('id', 'category', 'amount');
                 });
+
+                resCategory = res.body.categories[0];
+
+                return Category.findById(resCategory.id);
+            })
+            .then(category => {
+                category.id.should.equal(resCategory.id);
+                category.category.should.equal(resCategory.category);
+                category.amount.should.equal(resCategory.amount);
+                category.user_id.should.equal(user.id);
+            });
         });
     });
 });
