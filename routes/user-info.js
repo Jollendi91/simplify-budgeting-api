a4ebbb7d6102ec5ecfa0f4aacbebdd1e0bf40e18
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-04 10:34:27 -0500

    create protected user route

---
+++ routes/user-info.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const {User} = require('../models');
+
+router.get('/', (req, res) => User.findById(req.user.id)
+    .then(user => res.json(user.apiRepr()))
+);
+
+router.put('/', (req, res) => {
+    if(!(req.user.id && req.body.id && req.user.id === req.body.id.toString())) {
+        const message = (`Request user id (${req.user.id}) and request body id (${req.body.id}) must match`);
+
+        console.error(message);
+        res.status(400).json({message});
+    }
+
+    const toUpdate = {};
+    const updateableFields = ['setupStep', 'monthlySalary'];
+
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            toUpdate[field] = req.body[field];
+        }
+    });
+
+    return User.update(toUpdate, {
+        where: {
+            id: req.user.id
+        }
+    })
+    .then(() => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+module.exports = router;
