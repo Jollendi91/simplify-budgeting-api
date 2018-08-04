abfcf9291447cc6658697743363d0a29e0e8035c
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-04 09:02:52 -0500

    create custom error handler

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+
+const jwtAuth = require('./middleware/jwt-auth');
+
 const userRouter = require('./routes/users');
 const billsRouter = require('./routes/bills');
 const categoryRouter = require('./routes/categories');
@@ -14,8 +17,26 @@ app.use(morgan('common'));
 app.use(express.json());
 
 
+// Protected Routes
 app.use('/user', userRouter);
 app.use('/bills', billsRouter);
 app.use('/categories', categoryRouter);
+
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+//Custom Error Handler
+app.use((err, req, res, next) => {
+    if (err.status) {
+        const errBody = Object.assign({}, err, {nessage: err.message});
+        res.status(err.status).json(errBody);
+    } else {
+        res.status(500).json({message: 'Internal Server Error'});
+        console.error(err);
+    }
+});
 
 module.exports = app;
